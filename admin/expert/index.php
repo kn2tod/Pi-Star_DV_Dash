0cd5da5971b33e88b4f38404d4d0a39e1a3d00ef
root <root@pi-star>
2024-07-27 12:40:35 -0400

    add info to web page meta data

--- admin/expert/index.php
+++ admin/expert/index.php
@@ -1,13 +1,21 @@
 <?php
 // Load the language support
+require_once('../config/version.php');
 require_once('../config/language.php');
 require_once('../config/ircddblocal.php');
+$progname = basename($_SERVER['SCRIPT_FILENAME'],".php");
+$rev=$version;
 $MYCALL=strtoupper($callsign);
 $MYHOST=php_uname('n');
+$Debian=exec('sed -n "s/VERSION_CODENAME=\(.*\)/\u\1/p" /etc/os-release');
+$Linux=php_uname('s')." ".php_uname('r')." ".php_uname('v')." ".php_uname('m');
+$Hardware=exec('sed -n "s|^Model.*: Raspberry \(.*\)|\1|p" /proc/cpuinfo');
+
 //Load the Pi-Star Release file
 $pistarReleaseConfig = '/etc/pistar-release';
 $configPistarRelease = array();
 $configPistarRelease = parse_ini_file($pistarReleaseConfig, true);
+$ver=$configPistarRelease['Pi-Star']['Version'];
 //Load the Version Info
 require_once('../config/version.php');
 ?>
@@ -19,7 +27,11 @@ require_once('../config/version.php');
     <meta name="robots" content="follow" />
     <meta name="language" content="English" />
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-    <meta name="Author" content="Andrew Taylor (MW0MWZ)" />
+    <?php echo "<meta name=\"generator\" content=\"$progname $rev\" />\n"; ?>
+    <?php echo "<meta name=\"system\" content=\"$Debian $Linux\" />\n"; ?>
+    <?php echo "<meta name=\"platform\" content=\"$Hardware\" />\n"; ?>
+    <?php echo "<meta name=\"version\" content=\"Pi-Star: $ver - $rev\" />\n"; ?>
+    <meta name="Author" content="Andy Taylor (MW0MWZ), Mark Prichard (KN2TOD)" />
     <meta name="Description" content="Pi-Star Expert Editor" />
     <meta name="KeyWords" content="Pi-Star" />
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
@@ -58,6 +70,7 @@ require_once('../config/version.php');
 
 <div class="footer">
 Pi-Star / Pi-Star Dashboard, &copy; Andy Taylor (MW0MWZ) 2014-<?php echo date("Y"); ?>.<br />
+Further enhancements by Mark Prichard (KN2TOD), <br />
 Need help? Click <a style="color: #ffffff;" href="https://www.facebook.com/groups/pistarusergroup/" target="_new">here for the Support Group</a><br />
 Get your copy of Pi-Star from <a style="color: #ffffff;" href="http://www.pistar.uk/downloads/" target="_new">here</a>.<br />
 </div>
