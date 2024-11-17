3a78e091cb484f08537266313b54ac5dcf8d7932
root <root@pi-star>
2024-11-17 16:17:21 -0500

    fix response width

--- admin/expert/jitter_test.php
+++ admin/expert/jitter_test.php
@@ -146,10 +146,10 @@ if ($_SERVER["PHP_SELF"] == "/admin/expert/jitter_test.php") {
   </form>
   </td></tr>
 
-  <div class="contentwide" style="width:100%">
-  <table width="100%">
+  <div class="contentwide">
+  <table width="100%" style="font-size:88%">
   <tr><th>Results</th></tr>
-  <tr><td align="left"><div id="tail" style="width:100%">Starting jitter tests, please wait...<br /></div></td></tr>
+  <tr><td align="left"><div id="tail" style="width:auto">Starting jitter tests, please wait...<br /></div></td></tr>
   </table>
   </div>
 
