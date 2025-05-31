4eff5a3a80f975ec1faf876b27400db8e4058fc1
root <root@pi-star>
2025-05-31 17:59:26 -0400

    chg table role

--- admin/sysinfo.php
+++ admin/sysinfo.php
@@ -298,12 +298,15 @@ echo "  </tbody>\n";
 ?>
   </table>
   </div>
+  <footer aria-label="footer">
   <div class="footer">
   Pi-Star web config, &copy; Andy Taylor (MW0MWZ) 2014-<?php echo date("Y"); ?>.<br />
+  Further enhancements by Mark Prichard (KN2TOD)<br />
   Need help? Click <a style="color: #ffffff;" href="https://www.facebook.com/groups/pistarusergroup/" target="_new">here for the Support Group</a><br />
   Get your copy of Pi-Star from <a style="color: #ffffff;" href="http://www.pistar.uk/downloads/" target="_blank">here</a>.<br />
   <br />
   </div>
+  </footer>
   </div>
   </body>
   </html>
